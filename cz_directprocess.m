4b19035f06bf7ba097c203a5ddbe824397097ff6
chzhuo158 <32688745+chzhuo158@users.noreply.github.com>
2017-10-12 23:56:33 -0400

    Revert "Initial commit"
    This reverts commit c4a9a2c934ef97d6ab4d44beb36c9fb4b799a7aa.

--- cz_directprocess.m
+++
@@ -1,5 +0,0 @@
-root = 'D:\LabData\515Lab1-LabSection2-Group3\LabSection2_Group3';
-%filename = (fullfile(root,'datareadtest.txt'));
-filename = (fullfile(root,'Co-60','1','wave0.txt'));
-load(filename);
-[pk_amp,pk_locs]=cz_findpeak(wave0);
